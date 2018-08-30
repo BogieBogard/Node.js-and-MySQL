e7f544c8cb33c12d11f4480b65ed2d7a2c0594dc
BogieBogard <mbogard@utexas.edu>
2018-08-29 20:14:26 -0500

    bamazonCustomer.js logic finished for first item

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -77,51 +77,51 @@ function initialPrompt() {
 }
 
 function item1() {
-  inquirer
-    .prompt({
+  connection.query("SELECT * FROM products WHERE id = '1'", function(err, res) {
+    // Converting the price of the item from an array to an integer
+    let formattedPrice1 = JSON.stringify(res);
+    let formattedPrice2 = formattedPrice1.slice(91);
+    let formattedPrice3 = parseInt(formattedPrice2);
+    console.log("*** Please note that the price per unit is: $" + formattedPrice3 + " ***");
+  });
+  inquirer.prompt({
       name: "stock_quantity",
       type: "input",
       message: "What quantity of this item would you like to purchase?"
     })
     .then(function(answer) {
-      connection.query("SELECT * FROM products WHERE id = '1'", function(
-        err,
-        res
-      ) {
+      connection.query("SELECT * FROM products WHERE id = '1'", function(err, res) {
         if (err) throw err;
         // Converting the user's choice for the quantity desired
         let formattedAnswer1 = JSON.stringify(answer);
         let formattedAnswer2 = formattedAnswer1.slice(19);
         let formattedAnswer3 = parseInt(formattedAnswer2);
-        console.log(
-          "You have requested a total quantity of: " + formattedAnswer3
-        );
 
         // Converting the amount currently in stock from an array to an integer
         let formattedRes1 = JSON.stringify(res);
         let formattedRes2 = formattedRes1.slice(111);
         let formattedRes3 = parseInt(formattedRes2);
-        console.log("Number currently in stock: " + formattedRes3);
+
+        // We need to create the formatted price again since this function doesn't have access to the variable above
+        let formattedPrice1 = JSON.stringify(res);
+        let formattedPrice2 = formattedPrice1.slice(91);
+        let formattedPrice3 = parseInt(formattedPrice2);
 
         // If the user asks for a quantity that is greater than what is in stock then notify the user
         if (formattedAnswer3 > formattedRes3) {
-          console.log(
-            "Insufficent quantity! You asked for " +
-              formattedAnswer3 +
-              " but we only have " +
-              formattedRes3
-          );
-          connection.end();
+          console.log("Insufficent quantity! You asked for " + formattedAnswer3 +" but we only have " + formattedRes3);
+          console.log("Would you like to purchase something else?");
+          initialPrompt();
         } else {
           connection.query(
             "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = '1'",
             [formattedAnswer3],
             function(err, res) {
               if (err) throw err;
-              console.log(res);
+              let totalSpent = formattedAnswer3 * formattedPrice3;
+              console.log("Thank you for your order! Your payment method has been charged: $" + totalSpent);
             }
           );
-
           connection.end();
         }
       });
